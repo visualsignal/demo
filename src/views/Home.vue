9f232fe76ed98f67daa21a7202a67a7bc95731fe
Dave Blencowe <dblencowe@gmail.com>
2020-11-22 21:14:21 +0000

    Scaffold out home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,56 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>VisualSignal Demo Application</h1>
+    <p>Welcome to the VisualSignal demo application.</p>
+    <p>Enter a display name and room name to begin.</p>
+    <p>To connect with someone, have the come to the same url as you're on now and enter a matching
+      room name</p>
+    <form @submit.prevent="joinRoom"  class="form">
+      <div>
+        <label for="displayName">Display Name</label>
+        <input type="text" id="displayName" v-model="displayName">
+      </div>
+      <div>
+        <label for="roomName">Room Name</label>
+        <input type="text" id="roomName" v-model="roomName">
+      </div>
+      <button type="submit">Join Room</button>
+    </form>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      displayName: '',
+      roomName: '',
+    };
+  },
+  methods: {
+    joinRoom() {
+      console.log('connect to conductor and join room');
+    },
   },
 };
 </script>
+
+<style lang="scss">
+
+.form {
+  max-width: 40%;
+  margin: 0 auto;
+
+  input[type=text] {
+    width: 100%;
+    padding: 12px 20px;
+    margin: 8px 0;
+    box-sizing: border-box;
+  }
+}
+
+</style>
